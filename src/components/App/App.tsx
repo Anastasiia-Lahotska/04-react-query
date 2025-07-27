37d85ae660120d42d92a187b1db06853cff1897e
Anastasiia Lahotska <nastyalahotska@gmail.com>
2025-07-27 08:31:02 +0300

    -

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,62 +1,83 @@
-import { useState } from 'react';
-import { type Movie } from '../../types/movie';
-import { fetchMovies } from '../../services/movieService';
-import SearchBar from '../SearchBar/SearchBar';
-import toast, { Toaster } from 'react-hot-toast';
-import MovieGrid from '../MovieGrid/MovieGrid';
-import Loader from '../Loader/Loader';
-import ErrorMessage from '../ErrorMessage/ErrorMessage';
-import MovieModal from '../MovieModal/MovieModal';
-
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import MovieModal from "../MovieModal/MovieModal";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import Loader from "../Loader/Loader";
+import css from "./App.module.css";
+import toast from "react-hot-toast";
+import type { Movie } from "../../types/movie";
+import { useState } from "react";
+import { useEffect } from "react";
+import { fetchMovies } from "../../services/movieService";
+import { Toaster } from "react-hot-toast";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
+import ReactPaginate from "react-paginate";
 
 export default function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasError, setHasError] = useState(false);
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
- const handleSearch = async (query: string) => {
-  const trimmedQuery = query.trim();
+  const { data, isError, isSuccess, isFetching } = useQuery({
+    queryKey: ["movies", query, currentPage],
+    queryFn: () => fetchMovies(query, currentPage),
+    enabled: query !== "",
+    placeholderData: keepPreviousData,
+  });
 
-  if (!trimmedQuery) {
-    toast.error('Please enter your search query.');
-    return;
-  }
+  const handleSearch = (searchQuery: string) => {
+    setQuery(searchQuery);
+    setCurrentPage(1);
+  };
 
-  try {
-    setIsLoading(true);
-    setHasError(false);
-    setMovies([]);
-
-    const results = await fetchMovies(trimmedQuery);
-
-    if (results.length === 0) {
-      toast.error('No movies found for your request.');
+  useEffect(() => {
+    if (data?.results.length === 0) {
+      toast("No movies found for your request");
     }
+  }, [data]);
 
-    setMovies(results);
-  } catch {
-    setHasError(true);
-    toast.error('Something went wrong!');
-  } finally {
-    setIsLoading(false);
-  }
-};
+  const totalPages = data?.total_pages ?? 0;
+
+  const handleSelectedMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const closeModal = () => {
+    setSelectedMovie(null);
+  };
 
   return (
-    <>
-       <Toaster position='bottom-center' />
+    <div className={css.app}>
       <SearchBar onSubmit={handleSearch} />
 
-      {isLoading && <Loader />}
-      {hasError && <ErrorMessage />}
-      {!isLoading && !hasError && (
-        <MovieGrid movies={movies} onSelect={setSelectedMovie} />
+      <Toaster />
+
+      {isError && <ErrorMessage />}
+
+      {isSuccess && totalPages > 1 && (
+        <ReactPaginate
+          pageCount={totalPages}
+          pageRangeDisplayed={3}
+          marginPagesDisplayed={1}
+          onPageChange={({ selected }) => setCurrentPage(selected + 1)}
+          forcePage={currentPage - 1}
+          containerClassName={css.pagination}
+          activeClassName={css.active}
+          nextLabel="→"
+          previousLabel="←"
+          renderOnZeroPageCount={null}
+        />
+      )}
+
+      {query !== "" && isFetching && <Loader />}
+
+      {isSuccess && data.results.length > 0 && (
+        <MovieGrid onSelect={handleSelectedMovie} movies={data.results} />
       )}
 
       {selectedMovie && (
-        <MovieModal movie={selectedMovie} onClose={() => setSelectedMovie(null)} />
+        <MovieModal movie={selectedMovie} onClose={closeModal} />
       )}
-    </>
+    </div>
   );
 }
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,32 +1,31 @@
-import axios from 'axios';
-import { type Movie } from '../types/movie';
+import axios from "axios";
+import type { Movie } from "../types/movie";
 
 interface FetchMoviesResponse {
   results: Movie[];
+  total_pages: number;
+  page: number;
 }
 
-export async function fetchMovies(query: string): Promise<Movie[]> {
-  const config = {
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+
+
+export const fetchMovies = async (
+  query: string,
+  page: number
+): Promise<FetchMoviesResponse> => {
+
+  const response = await axios.get<FetchMoviesResponse>("/search/movie", {
     params: {
       query,
-      include_adult: false,
-      language: 'en-US',
-      page: 1,
+      page,
     },
     headers: {
-      accept: 'application/json',
       Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYWU5ZmU4ODViZjRmM2JlMGE4ZTBhZWQ1YThhNzYwMSIsIm5iZiI6MTc1MzU5MTM4My43OTYsInN1YiI6IjY4ODVhZTU3ZWU0Y2IwMWMzZmQ5YjI4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lTh3ruGlNrgY2mewL1Ls7cn0IsqJVGVhRnMf8DUK47I`,
+ 
+       accept: "application/json",
     },
-  };
+  });
 
-  try {
-    const response = await axios.get<FetchMoviesResponse>(
-      'https://api.themoviedb.org/3/search/movie',
-      config
-    );
-    return response.data.results;
-  } catch (error) {
-    console.error('Error fetching movies:', error);
-    throw error; 
-  }
-}
+  return response.data;
+};
